60aaf93518c25b5582a4e60a70e80547ccdacbf2
KVcraft <kavinduv69@gmail.com>
2025-04-15 12:56:57 +0530

    Add all files again

---
+++ src/main/java/com/urban_food/UrbanFoodApplication.java
@@ -0,0 +1,13 @@
+package com.urban_food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urban_food/UrbanFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urban_food;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
