b8bb6a7c33d07b3a57b4a2903dbb0f0e1bc291a4
Dils-beep <dilsw2511@gmail.com>
2025-04-18 13:07:55 +0530

    Created Customer Class

--- src/main/java/com/urban_food/entity/Customer.java
+++ src/main/java/com/urban_food/entity/Customer.java
@@ -1,4 +1,92 @@
 package com.urban_food.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Customer")
 public class Customer {
+    @Id
+    private String customerID;
+    private String customerUsername;
+    private String customerEmail;
+    private String customerAddress;
+    private String customerContact;
+    private String customerPassword;
+
+    // Constructors
+    public Customer() {
+    }
+
+    public Customer(String customerID, String customerUsername, String customerEmail,
+                    String customerAddress, String customerContact, String customerPassword) {
+        this.customerID = customerID;
+        this.customerUsername = customerUsername;
+        this.customerEmail = customerEmail;
+        this.customerAddress = customerAddress;
+        this.customerContact = customerContact;
+        this.customerPassword = customerPassword;
+    }
+
+    // Getters and Setters
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerContact() {
+        return customerContact;
+    }
+
+    public void setCustomerContact(String customerContact) {
+        this.customerContact = customerContact;
+    }
+
+    public String getCustomerPassword() {
+        return customerPassword;
+    }
+
+    public void setCustomerPassword(String customerPassword) {
+        this.customerPassword = customerPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerID='" + customerID + '\'' +
+                ", customerUsername='" + customerUsername + '\'' +
+                ", customerEmail='" + customerEmail + '\'' +
+                ", customerAddress='" + customerAddress + '\'' +
+                ", customerContact='" + customerContact + '\'' +
+                ", customerPassword='" + customerPassword + '\'' +
+                '}';
+    }
 }
