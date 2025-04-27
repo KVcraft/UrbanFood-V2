4080f4da353b1ccde1eb765c2a52e8aeb99bc387
Sahanya42 <sahanyayamini42@gmail.com>
2025-04-27 10:15:36 +0530

    Updated Cart Class

--- src/main/java/com/urban_food/controller/CartController.java
+++ src/main/java/com/urban_food/controller/CartController.java
@@ -16,6 +16,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
+    // API to add a new cart item
     @PostMapping
     public ResponseEntity<Void> addToCart(@RequestBody Cart cart) {
         try {
@@ -26,11 +27,13 @@ public class CartController {
         }
     }
 
+    // API to get all cart items
     @GetMapping
     public List<Cart> getAll() {
         return cartService.getAllCarts();
     }
 
+    // API to get a specific cart item by its ID
     @GetMapping("/{id}")
     public ResponseEntity<Cart> getById(@PathVariable String id) {
         return cartService.getCartById(id)
@@ -38,6 +41,7 @@ public class CartController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    // API to update a cart item by its ID
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable String id, @RequestBody Cart cart) {
         cart.setCartId(id);
@@ -45,19 +49,22 @@ public class CartController {
         return ResponseEntity.ok().build();
     }
 
+    // API to delete a cart item by its ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
         cartService.deleteCart(id);
         return ResponseEntity.noContent().build();
     }
+
+    // API to checkout the cart (simulate purchase)
     @PutMapping("/checkout/{cartId}")
     public ResponseEntity<Void> checkoutCart(@PathVariable String cartId) {
         try {
             cartService.checkoutCart(cartId);
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok().build(); // Return 200 OK if checkout successful
         } catch (Exception e) {
-            e.printStackTrace();
-            return ResponseEntity.status(500).build();
+            e.printStackTrace();  // Print exception details in server logs
+            return ResponseEntity.status(500).build(); // Return 500 Internal Server Error if exception occurs
         }
     }
 
